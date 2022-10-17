7ad29b7498a51e44c57b6e645e321683b6fc0cdb
Matheus Tavares <tavareszmatheusz@gmail.com>
2022-10-16 22:25:22 -0300

    Importando fonte

--- components/layout.js
+++ components/layout.js
@@ -12,7 +12,14 @@ export default function Layout({children, className}){
                 <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
                 <meta name="description" content="Compre hospedagens de alta performance com preço acessível"/>
                 <link rel="canonical" href="https://loup.host"/>
-
+                <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+        <link 
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600&display=swap"
+         rel="stylesheet"/>
                 <title>LoupHost - Hospedagem sem dor de cabeça</title>
             </Head>
             <div className={className}>
